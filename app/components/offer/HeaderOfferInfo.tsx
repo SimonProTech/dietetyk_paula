315341c293a04df546907347f7f1596116f6934f
Szymon <szymon98181@gmail.com>
2025-07-19 20:50:09 +0200

    clearance

--- app/components/offer/HeaderOfferInfo.tsx
+++ app/components/offer/HeaderOfferInfo.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
 import { usePathname } from 'next/navigation'
 import Consulting from '@/app/components/offer/Consulting'
 import FoodPlan from '@/app/components/offer/FoodPlan'
